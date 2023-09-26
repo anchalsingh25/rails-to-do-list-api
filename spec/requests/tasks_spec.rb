4c2d81b10529803747c1380916bd4eb3f15fd4b2
anchalsingh25 <anchalsingh1373@gmail.com>
2023-09-26 08:43:19 +0530

    factory bot added

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 describe 'task api', type: :request do
   it 'returns all tasks' do
+
+    FactoryBot.create(:task, title: "practice api gateway", description: "go through the aws api gateway");
+    FactoryBot.create(:task, title: "complete dsa", description: "solve dsa problem");
+
     get '/api/v1/tasks'
     expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body).size).to eq(2)
   end
 end
